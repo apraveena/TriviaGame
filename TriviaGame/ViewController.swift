62dc8d3772a6bb32f3654488b323bd3f26f61ffd
Praveena <praveena@spuriq.com>
2019-07-14 14:03:05 -0700

    Initial Commit

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by Spur IQ on 7/14/19.
+//  Copyright © 2019 Praveena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
